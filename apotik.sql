26ab8eb3c7f93a12d0c0387e9796c29241eac8ff
unknown <danybstian@gmail.com>
2019-12-12 02:11:27 +0700

    Purchased more than 1

--- apotik.sql
+++ apotik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2019 at 01:02 AM
+-- Generation Time: Dec 11, 2019 at 08:10 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -37,6 +37,16 @@ CREATE TABLE `obat_terjual` (
   `jumlah` int(11) NOT NULL,
   `total_harga` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `obat_terjual`
+--
+
+INSERT INTO `obat_terjual` (`row_id`, `no_faktur`, `type_penjualan`, `kode_obat`, `nama_obat`, `jumlah`, `total_harga`) VALUES
+(1, 1, 'with-resep', 1, 'Ambroxol', 2, 14000),
+(2, 1, 'with-resep', 10, 'kapsida', 3, 45000),
+(3, 1, 'non-resep', 12, 'antalgin', 2, 7000),
+(4, 1, 'non-resep', 15, 'Paracetamol', 3, 15000);
 
 -- --------------------------------------------------------
 
@@ -113,15 +123,15 @@ CREATE TABLE `pemasukan_obat` (
 --
 
 INSERT INTO `pemasukan_obat` (`kode_obat`, `nama_obat`, `jenis_obat`, `harga`, `stok_obat`, `tgl_masuk`, `tgl_kadaluarsa`, `kategori_obat`) VALUES
-('1', 'Ambroxol', 'Pil', 7000, 70, '2019-09-02', '2021-10-02', NULL),
-('10', 'kapsida', 'Kapsul', 15000, 25, '2019-10-13', '2025-10-13', NULL),
-('11', 'asam mefenamat', 'Pil', 5000, 54, '2019-10-16', '2023-11-22', NULL),
+('1', 'Ambroxol', 'Pil', 7000, 52, '2019-09-02', '2021-10-02', NULL),
+('10', 'kapsida', 'Kapsul', 15000, 8, '2019-10-13', '2025-10-13', NULL),
+('11', 'asam mefenamat', 'Pil', 5000, 51, '2019-10-16', '2023-11-22', NULL),
 ('113', 'Epexone', 'Tablet', 35000, 35, '2019-11-27', '2019-11-21', NULL),
-('12', 'antalgin', 'Kapsul', 3500, 58, '2019-09-29', '2025-09-30', NULL),
-('1212', 'antimo', 'Kapsul', 5000, 97, '2019-08-03', '2019-08-16', NULL),
+('12', 'antalgin', 'Kapsul', 3500, 50, '2019-09-29', '2025-09-30', NULL),
+('1212', 'antimo', 'Kapsul', 5000, 94, '2019-08-03', '2019-08-16', NULL),
 ('13', 'cendo xytrol eye', 'Obat Tetes', 53000, 9, '2019-08-05', '2023-09-20', NULL),
 ('14', 'Pct infus', 'Infusa', 20000, 15, '2019-10-02', '2021-10-02', NULL),
-('15', 'Paracetamol', 'Pil', 5000, 27, '2019-09-24', '2020-09-24', NULL),
+('15', 'Paracetamol', 'Pil', 5000, 0, '2019-09-24', '2020-09-24', NULL),
 ('16', 'sanmol', 'Syirup', 15000, 33, '2019-09-30', '2024-10-02', NULL),
 ('17', 'Omeprazole', 'Kapsul', 28000, 38, '2019-08-29', '2026-09-29', NULL),
 ('18', 'hydrocortisone', 'Salep', 6000, 15, '2019-08-27', '2027-09-19', NULL),
@@ -133,7 +143,7 @@ INSERT INTO `pemasukan_obat` (`kode_obat`, `nama_obat`, `jenis_obat`, `harga`, `
 ('3', 'natrium diklofenak', 'Pil', 7000, 19, '2019-10-01', '2023-09-30', NULL),
 ('4', 'ampicilin', 'Kaplet', 7000, 69, '2019-09-29', '2022-10-06', NULL),
 ('5', 'antasida doen', 'Tablet', 4000, 80, '2019-09-29', '2023-10-18', NULL),
-('6', 'promag', 'Tablet', 5000, 39, '2019-08-30', '0000-00-00', NULL),
+('6', 'promag', 'Tablet', 5000, 36, '2019-08-30', '0000-00-00', NULL),
 ('7', 'Garam Oralit', 'Serbuk', 3000, 25, '2019-10-06', '0000-00-00', NULL),
 ('8', 'asam askorbat (vitamin c)', 'Pil', 3000, 45, '2019-10-08', '0000-00-00', NULL),
 ('9', 'vitamin B kompleks', 'Pil', 3000, 64, '2019-10-08', '2019-09-14', NULL);
@@ -146,13 +156,19 @@ INSERT INTO `pemasukan_obat` (`kode_obat`, `nama_obat`, `jenis_obat`, `harga`, `
 
 CREATE TABLE `penjualan_resep_detail` (
   `no_faktur` int(10) NOT NULL,
-  `kode_obat` varchar(10) NOT NULL,
   `nama_pasien` varchar(30) NOT NULL,
   `nama_dokter` varchar(30) NOT NULL,
   `tgl` date NOT NULL,
   `jumlah` int(11) NOT NULL,
   `total` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penjualan_resep_detail`
+--
+
+INSERT INTO `penjualan_resep_detail` (`no_faktur`, `nama_pasien`, `nama_dokter`, `tgl`, `jumlah`, `total`) VALUES
+(1, 'Dandi', 'Bastian', '2019-12-12', 5, 59000);
 
 -- --------------------------------------------------------
 
@@ -166,6 +182,13 @@ CREATE TABLE `penjualan_tanparesep_detail` (
   `tgl` date NOT NULL,
   `total1` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penjualan_tanparesep_detail`
+--
+
+INSERT INTO `penjualan_tanparesep_detail` (`no_faktur1`, `jumlah`, `tgl`, `total1`) VALUES
+(1, 5, '2019-12-12', 22000);
 
 -- --------------------------------------------------------
 
@@ -234,19 +257,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `obat_terjual`
 --
 ALTER TABLE `obat_terjual`
-  MODIFY `row_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `row_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `penjualan_resep_detail`
 --
 ALTER TABLE `penjualan_resep_detail`
-  MODIFY `no_faktur` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `no_faktur` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `penjualan_tanparesep_detail`
 --
 ALTER TABLE `penjualan_tanparesep_detail`
-  MODIFY `no_faktur1` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `no_faktur1` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
